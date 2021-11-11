7b5ebd07f78e172bfc661aebf7dbf9d036b1e318
Sunaif_adkar <80641581+sunaif-adkar2@users.noreply.github.com>
2021-11-11 08:03:06 +0530

    Create __main__.py

---
+++ SaBots/__main__.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @AlbertEinsteinTG
+# (e) @sunaif_adkar 
+# Copyright permission under MIT License
+# All rights reserved by PR0FESS0R-99
+# License -> https://github.com/sunaif-adkar2/auto-filter-bot-premier2/blob/main/LICENSE
+
+from .donlee_robot import DonLee_Robot
+app = DonLee_Robot()
+app.run()
