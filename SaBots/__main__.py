2ec0d40f4ab119c1b688f9fb2a044e456e590f4e
Sunaif_adkar <80641581+sunaif-adkar2@users.noreply.github.com>
2021-11-11 09:56:22 +0530

    Create __main__.py

---
+++ SaBots/__main__.py
@@ -0,0 +1,10 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @AlbertEinsteinTG
+# (e) @sunaif_adkar 
+# Copyright permission under MIT License
+# All rights reserved by sunaif Adkar
+# change name's if you edit thua repo dont change anything else
+from .SaBots import SaBots
+app = SaBots()
+app.run()
